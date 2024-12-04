e196916078268941f9956199db125d70c76d9192
kazusa <i@kazusa.me>
2024-12-04 18:54:05 +0800

    Wed Dec  4 06:54:05 PM CST 2024

---
+++ 57.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int n, m;
+
+int main(int, char const *[]) {
+    std::cin >> n >> m;
+    std::vector<int> dp(max(n + 1, 1), 0); dp[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        dp[i] = accumulate(dp.begin() + max(0, i - m), dp.begin() + i, 0);
+    }
+    for (const auto &el : dp) cout << el << " "; cout << endl;
+    cout << dp[n] << endl;
+    return 0;
+}
